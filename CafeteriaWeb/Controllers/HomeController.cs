d3e1aaa6e9573205a9a58f2bf85f82c9913a0e88
Juan Pablo Martinez <jpms19992015@gmail.com>
2023-06-16 20:11:55 -0600

    upload first

---
+++ CafeteriaWeb/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using CafeteriaWeb.Entities;
+using CafeteriaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeteriaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(ClientesEnt entitad)
+        {
+            ClientesModel model = new ClientesModel();
+            model.IniciarSesion(entitad);
+
+            return RedirectToAction("Inicio", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarClientes(ClientesEnt entidad)
+        {
+
+            ClientesModel model = new ClientesModel();
+            model.RegistrarClientes(entidad);
+
+            return View("Login", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ CafeteriaWeb/Models/ClientesModel.cs
@@ -0,0 +1,31 @@
+﻿using CafeteriaWeb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeteriaWeb.Models
+{
+    public class ClientesModel
+    { 
+
+
+        public void IniciarSesion(ClientesEnt entitad)
+    {
+
+        //Mandamos a llamar a la base de datos
+        //Mandamos a llamar a la web api
+
+
+    }
+
+    public void RegistrarClientes(ClientesEnt entidad)
+    {
+
+        //Mandamos a llamar a la base de datos
+        //Mandamos a llamar a la web api
+
+
+    }
+}
+}
